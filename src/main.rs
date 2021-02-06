540178af39ddd886e12c99ad9178a254ffd9883f
raunakab <rabhagat31@gmail.com>
2021-02-06 00:54:17 -0700

    minor changes to bootstart to ec2

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,40 @@
 #![allow(unused)]
 
-#[macro_use]
-extern crate diesel;
-
 use actix_web::{
     web,
     App,
     HttpServer,
-    dev::ServiceRequest,
+    dev::{
+        ServiceRequest,
+        HttpResponseBuilder,
+    },
     Error,
-};
-use diesel::prelude::*;
-use diesel::r2d2::{
-    self,
-    ConnectionManager,
+    error,
+    HttpResponse,
 };
 
-mod handlers;
-mod errors;
-mod models;
-mod schema;
-
-pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
+pub async fn hello_world() -> Result<HttpResponse, Error> {
+    return Ok(
+        web::block(move || -> Result<String, ()> {
+            return Ok(String::from("Hello, world!\n"));
+        })
+            .await
+            .map::<HttpResponse, fn(String) -> HttpResponse>(|msg: String| -> HttpResponse {
+                return HttpResponse::Ok().json(msg);
+            })
+            .map_err(|_: error::BlockingError<()>| -> HttpResponseBuilder {
+                return HttpResponse::InternalServerError();
+            })?
+    );
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    dotenv::dotenv().ok();
-    std::env::set_var("RUST_LOG", "actix_web=debug");
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
-    let manager: ConnectionManager::<PgConnection> = ConnectionManager::<PgConnection>::new(database_url);
-    let pool: Pool = r2d2::Pool::builder()
-    .build(manager)
-    .expect("Failed to create pool.");
-
     return HttpServer::new(move || {
         return App::new()
-        .data(pool.clone())
-        .route("/users",      web::get()    .to(handlers::get_users))
-        .route("/users/{id}", web::get()    .to(handlers::get_user_by_id))
-        .route("/users",      web::post()   .to(handlers::add_user))
-        .route("/users/{id}", web::delete() .to(handlers::delete_user));
+            .route("/", web::get()  .to(hello_world));
     })
+    // .bind("34.210.113.149")?
     .bind("127.0.0.1:8080")?
     .run()
     .await;
